2e777be291f58069b6a13c286c0ffc3ef655deca
Alex Billson <alex.billson@sky.uk>
2024-01-19 15:41:39 +0000

    remove console log

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { HeroSection } from "./components/HeroSection";
 export const App = () => {
-  console.log("weird");
   return (
     <main className="App">
       <HeroSection />
